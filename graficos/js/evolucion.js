c708c62784c13d1c34ae0537636a66789255e49a
Oscar Roca <oscar.1995.roca@gmail.com>
2019-06-12 22:39:28 -0400

    12-06-2019 - 22:40

---
+++ graficos/js/evolucion.js
@@ -0,0 +1,24 @@
+var omision = document.getElementById('omision').getContext('2d');
+
+
+
+let chart = new Chart(omision, {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'First dataset',
+            data: [0, 20, 40, 50]
+        }],
+        labels: ['January', 'February', 'March', 'April']
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 50,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
+});

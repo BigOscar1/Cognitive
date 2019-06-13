9b757952f61d376b40161235ad6cefe22469ffdc
Oscar <oscar.1995.roca@gmail.com>
2019-06-13 02:10:12 -0400

    13-06-2019

--- graficos/js/evolucion.js
+++ graficos/js/evolucion.js
@@ -1,24 +1,47 @@
-var omision = document.getElementById('omision').getContext('2d');
+var evolucion = document.getElementById('evolucion').getContext('2d');
 
 
 
-let chart = new Chart(omision, {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: 'First dataset',
-            data: [0, 20, 40, 50]
-        }],
-        labels: ['January', 'February', 'March', 'April']
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 50,
-                    suggestedMax: 100
-                }
-            }]
+generarGrafica = (resultados) => {
+    let valor = '';
+    let chart = new Chart(evolucion, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Resultados',
+                data: function(){
+                    for(let v = 0; v < resultados.length; v++){
+                    valor += resultados[v]+ ',' ;
+                    return [valor.substring()]
+                }},
+                backgroundColor: [
+                    'rgba(255, 162, 0, 1)',
+                    'rgba(255, 162, 0, 1)',
+                    'rgba(255, 162, 0, 1)',
+                    'rgba(255, 162, 0, 1)',
+                    'rgba(255, 162, 0, 1)',                    
+                ],
+            }],
+            labels: ['January', 'February', 'March', 'April','mayo']
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 80,
+                        suggestedMax: 100
+                    }
+                }]
+            },
+            legend: {
+                display: false,
+            },
         }
-    }
-});
+    });
+}
+datos = [1,5,3,8];
+datos.map(function(x){
+    
+})
+
+generarGrafica([50,10,60,80,20])
